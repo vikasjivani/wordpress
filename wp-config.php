978a14a3d63b5d26662f1014e10e1a11bb22226f
vji-axelor <vji.axelor@gmail.com>
2020-09-12 17:04:45 +0530

    wordpress

---
+++ wp-config.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Grundeinstellungen für WordPress
+ *
+ * Zu diesen Einstellungen gehören:
+ *
+ * * MySQL-Zugangsdaten,
+ * * Tabellenpräfix,
+ * * Sicherheitsschlüssel
+ * * und ABSPATH.
+ *
+ * Mehr Informationen zur wp-config.php gibt es auf der
+ * {@link https://codex.wordpress.org/Editing_wp-config.php wp-config.php editieren}
+ * Seite im Codex. Die Zugangsdaten für die MySQL-Datenbank
+ * bekommst du von deinem Webhoster.
+ *
+ * Diese Datei wird zur Erstellung der wp-config.php verwendet.
+ * Du musst aber dafür nicht das Installationsskript verwenden.
+ * Stattdessen kannst du auch diese Datei als wp-config.php mit
+ * deinen Zugangsdaten für die Datenbank abspeichern.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL-Einstellungen ** //
+/**   Diese Zugangsdaten bekommst du von deinem Webhoster. **/
+
+/**
+ * Ersetze datenbankname_hier_einfuegen
+ * mit dem Namen der Datenbank, die du verwenden möchtest.
+ */
+define( 'DB_NAME', 'wordpress' );
+
+/**
+ * Ersetze benutzername_hier_einfuegen
+ * mit deinem MySQL-Datenbank-Benutzernamen.
+ */
+define( 'DB_USER', 'root' );
+
+/**
+ * Ersetze passwort_hier_einfuegen mit deinem MySQL-Passwort.
+ */
+define( 'DB_PASSWORD', '000000' );
+
+/**
+ * Ersetze localhost mit der MySQL-Serveradresse.
+ */
+define( 'DB_HOST', 'localhost' );
+
+/**
+ * Der Datenbankzeichensatz, der beim Erstellen der
+ * Datenbanktabellen verwendet werden soll
+ */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/**
+ * Der Collate-Type sollte nicht geändert werden.
+ */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Sicherheitsschlüssel
+ *
+ * Ändere jeden untenstehenden Platzhaltertext in eine beliebige,
+ * möglichst einmalig genutzte Zeichenkette.
+ * Auf der Seite {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * kannst du dir alle Schlüssel generieren lassen.
+ * Du kannst die Schlüssel jederzeit wieder ändern, alle angemeldeten
+ * Benutzer müssen sich danach erneut anmelden.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '6b>5a+^_O0Yv/T}}3bjQknB,fZ[=%>[b+CHf:Xn@ng0CJwwN9>_8eN-N<!J3m$sk' );
+define( 'SECURE_AUTH_KEY',  'S!Wy/dirRu|$hrsxk/Fd)[%L_9UE5mhE2S^sBmx;(@s;[jnM1%J;aZ<yUhgFTb4M' );
+define( 'LOGGED_IN_KEY',    'yB#PE-nV6qla4%%H6f3xyA6PGRDiy!+4h}m|@J]Lf3i.cT0Y.}:Z?~L<kHhvxvQL' );
+define( 'NONCE_KEY',        'E)jtV /?X^K7UJ8W9>ZkM+&?_Im>QQFP}T=&+@k8:`4gGy1pYM(aB38FWV>jbHx#' );
+define( 'AUTH_SALT',        '-|;@_h-nK~Fsw0nLXVk@!mX`V4qAuqB}BxR0;x+@h81s[0]@@=aAmr(<IEX$fvax' );
+define( 'SECURE_AUTH_SALT', 'l-TAGZATun }<VZ-(%bqAJvr6nkvTmq3aQOa(xWj>L^>^n?8L3iFlCw5a%1W:&<e' );
+define( 'LOGGED_IN_SALT',   'r?0gTs{9?G7<P4Yjzo5>gJ<{#$(Du{NO3UHDQcE|lieZBa%djHL9 %Nbv2CN)jm[' );
+define( 'NONCE_SALT',       '>_2dc5}Hm*Q7Q[pdPt9JZmYZ^*jh8x<e jx>e9y1?X{QH@=)T!,dzV/4&K<| :#.' );
+
+/**#@-*/
+
+/**
+ * WordPress Datenbanktabellen-Präfix
+ *
+ * Wenn du verschiedene Präfixe benutzt, kannst du innerhalb einer Datenbank
+ * verschiedene WordPress-Installationen betreiben.
+ * Bitte verwende nur Zahlen, Buchstaben und Unterstriche!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Für Entwickler: Der WordPress-Debug-Modus.
+ *
+ * Setze den Wert auf „true“, um bei der Entwicklung Warnungen und Fehler-Meldungen angezeigt zu bekommen.
+ * Plugin- und Theme-Entwicklern wird nachdrücklich empfohlen, WP_DEBUG
+ * in ihrer Entwicklungsumgebung zu verwenden.
+ *
+ * Besuche den Codex, um mehr Informationen über andere Konstanten zu finden,
+ * die zum Debuggen genutzt werden können.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* Das war’s, Schluss mit dem Bearbeiten! Viel Spaß. */
+/* That's all, stop editing! Happy publishing. */
+
+/** Der absolute Pfad zum WordPress-Verzeichnis. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Definiert WordPress-Variablen und fügt Dateien ein.  */
+require_once( ABSPATH . 'wp-settings.php' );
